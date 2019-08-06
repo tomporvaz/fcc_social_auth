f94316f32d60f1e4f243a2173adaeaa9d442a9df
tomporvaz <tomporvaz@gmail.com>
2019-08-05 21:07:02 -0400

    Added github strategy with no database.

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const session     = require('express-session');
 const mongo       = require('mongodb').MongoClient;
 const passport    = require('passport');
+const GitHubStrategy = require('passport-github').Strategy;
 
 const app = express();
 
@@ -18,83 +19,95 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'pug')
 
 mongo.connect(process.env.DATABASE, (err, db) => {
-    if(err) {
-        console.log('Database error: ' + err);
-    } else {
-        console.log('Successful database connection');
-      
-        app.use(session({
-          secret: process.env.SESSION_SECRET,
-          resave: true,
-          saveUninitialized: true,
-        }));
-        app.use(passport.initialize());
-        app.use(passport.session());
-      
-        function ensureAuthenticated(req, res, next) {
-          if (req.isAuthenticated()) {
-              return next();
-          }
-          res.redirect('/');
-        };
-
-        passport.serializeUser((user, done) => {
-          done(null, user.id);
-        });
-
-        passport.deserializeUser((id, done) => {
-            db.collection('socialusers').findOne(
-                {id: id},
-                (err, doc) => {
-                    done(null, doc);
-                }
-            );
-        });
-
-      
-        /*
-        *  ADD YOUR CODE BELOW
-        */
-
-        app.route('/auth/github').get(passport.authenticate('github'));
-      
-        app.route('/auth/github/callback')
-        .get(passport.authenticate('github', {failureRedirect: '/'}),
-          (req, res) => res.redirect('/profile')
+  if(err) {
+    console.log('Database error: ' + err);
+  } else {
+    console.log('Successful database connection');
+    
+    app.use(session({
+      secret: process.env.SESSION_SECRET,
+      resave: true,
+      saveUninitialized: true,
+    }));
+    app.use(passport.initialize());
+    app.use(passport.session());
+    
+    function ensureAuthenticated(req, res, next) {
+      if (req.isAuthenticated()) {
+        return next();
+      }
+      res.redirect('/');
+    };
+    
+    passport.serializeUser((user, done) => {
+      done(null, user.id);
+    });
+    
+    passport.deserializeUser((id, done) => {
+      db.collection('socialusers').findOne(
+        {id: id},
+        (err, doc) => {
+          done(null, doc);
+        }
         );
+      });
+      
+      
+      /*
+      *  ADD YOUR CODE BELOW
+      */
+      
+      passport.use(new GitHubStrategy({
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: 'https://toms-fcc-social-auth.glitch.me/auth/github/callback'
+      },
+      function(accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+        //Database logic here with callback containing our user object
+      }
+      ));
+      
+      app.route('/auth/github').get(passport.authenticate('github'));
+      
+      app.route('/auth/github/callback')
+      .get(passport.authenticate('github', {failureRedirect: '/'}),
+      (req, res) => res.redirect('/profile')
+      );
       
       
       
       
       
-        /*
-        *  ADD YOUR CODE ABOVE
-        */
+      /*
+      *  ADD YOUR CODE ABOVE
+      */
       
       
-        app.route('/')
-          .get((req, res) => {
-            res.render(process.cwd() + '/views/pug/index');
-          });
-
-        app.route('/profile')
-          .get(ensureAuthenticated, (req, res) => {
-               res.render(process.cwd() + '/views/pug/profile', {user: req.user});
-          });
-
-        app.route('/logout')
-          .get((req, res) => {
-              req.logout();
-              res.redirect('/');
-          });
-
-        app.use((req, res, next) => {
-          res.status(404)
-            .type('text')
-            .send('Not Found');
-        });
+      app.route('/')
+      .get((req, res) => {
+        res.render(process.cwd() + '/views/pug/index');
+      });
       
-        app.listen(process.env.PORT || 3000, () => {
-          console.log("Listening on port " + process.env.PORT);
-        });  
-}});
+      app.route('/profile')
+      .get(ensureAuthenticated, (req, res) => {
+        res.render(process.cwd() + '/views/pug/profile', {user: req.user});
+      });
+      
+      app.route('/logout')
+      .get((req, res) => {
+        req.logout();
+        res.redirect('/');
+      });
+      
+      app.use((req, res, next) => {
+        res.status(404)
+        .type('text')
+        .send('Not Found');
+      });
+      
+      app.listen(process.env.PORT || 3000, () => {
+        console.log("Listening on port " + process.env.PORT);
+      });  
+    }});
+    
